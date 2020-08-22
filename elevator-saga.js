894f0c3269a2371e15f2c5d89ee3f90fbdd75bd2
Claudio Rivera <me@claudiorivera.com>
2020-08-22 11:00:24 -0500

    passing challenge 1

--- elevator-saga.js
+++ elevator-saga.js
@@ -1,19 +1,30 @@
 {
   init: function(elevators, floors) {
-    elevators.forEach(elevator=>{
+      // Map through elevators, since there will be more in the future
+      elevators.map(elevator=>{
+          elevator.on("idle", function() {
+              // Check to see if anyone inside wants to go somewhere
+              if(elevator.getPressedFloors().length > 0) {
+                  // For now, go to each of the called floors
+                  // TODO: Add logic to sort floors more smart-like
+                  elevator.getPressedFloors().map(floor=>{
+                      elevator.goToFloor(floor);
+                  })
+              }
 
-      elevator.on("idle", function() {
+              // Otherwise, check to see if any floor is calling and go there
+              floors.map(floor=>{
+                  floor.on("down_button_pressed", ()=> {
+                      elevator.goToFloor(floor.floorNum())
+                  })
+                  floor.on("up_button_pressed", ()=> {
+                      elevator.goToFloor(floor.floorNum())
+                  })
+              })
+          });
 
-      });
-
-
-    })
-
-    floors.forEach(floor=>{
-      console.log(floor.floorNum());
-    })
-
+      })
   },
-  update: function(dt, elevators, floors) {
-  }
+      update: function(dt, elevators, floors) {
+      }
 }
